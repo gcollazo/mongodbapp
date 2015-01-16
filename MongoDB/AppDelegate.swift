8a39a694685b0662146e04d01a701ef0e655cbbd
Giovanni Collazo <hello@gcollazo.com>
2015-01-16 12:36:23 -0400

    Initial Commit

---
+++ MongoDB/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MongoDB
+//
+//  Created by Giovanni Collazo on 1/16/15.
+//  Copyright (c) 2015 Giovanni Collazo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
