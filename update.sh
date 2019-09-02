43d55de36663d688498f169aa425c8e661b5f2fe
Giovanni Collazo <hello@gcollazo.com>
2019-09-02 11:29:58 -0400

    Fix update script

--- update.sh
+++ update.sh
@@ -11,8 +11,8 @@ echo "--> Getting version numbers"
 
 CURR_VERSION=$(curl -s https://gcollazo.github.io/mongodbapp/ | grep -o '<div class="current-version">v.*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*-build\.[0-9]*')
 
-CURR_MONGO=$(echo $CURR_VERSION | grep -o '^[0-9]*\.[0-9]*\.[0-9]*')
-CURR_BUILD=$(echo $CURR_VERSION | grep -o '[0-9]*$')
+CURR_MONGO=$(echo "$CURR_VERSION" | grep -o '^[0-9]*\.[0-9]*\.[0-9]*')
+CURR_BUILD=$(echo "$CURR_VERSION" | grep -o '[0-9]*$')
 
 echo " -- Current mongodb.app version: $CURR_MONGO"
 
@@ -20,7 +20,7 @@ echo " -- Current mongodb.app version: $CURR_MONGO"
 # =========================== LATEST VERSION INFO ==============================
 # Get latest mongodb Production Relase version
 VERSION=$(curl -s https://www.mongodb.com/download-center/community | \
-  grep -o '<option value="1" selected>.* (current release)</option>' | \
+  grep -o '<option value="0" selected>.* (current release)</option>' | \
   grep -o '[0-9]*\.[0-9]*\.[0-9]*')
 echo " -- Latest mongodb version: $VERSION"
 
@@ -35,7 +35,7 @@ fi
 
 # =========================== DOWNLOAD =========================================
 echo '--> Download'
-DOWNLOAD_URL="https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-$VERSION.tgz"
+DOWNLOAD_URL="https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-$VERSION.tgz"
 
 # Download latest mongodb for mac
 echo " -- Downloading: $DOWNLOAD_URL"
@@ -43,11 +43,11 @@ curl -o /tmp/mongodb.tgz $DOWNLOAD_URL
 
 # Clean old mongodb dir
 echo " -- Cleaning directory $(pwd)/Vendor/mongodb"
-rm -rf $(pwd)/Vendor/mongodb
+rm -rf "$(pwd)/Vendor/mongodb"
 
 # Create dir
 echo " -- Creating directory $(pwd)/Vendor/mongodb"
-mkdir -p $(pwd)/Vendor/mongodb
+mkdir -p "$(pwd)/Vendor/mongodb"
 
 # Extract
 echo " -- Unzipping..."
@@ -94,9 +94,9 @@ echo " -- Build completed!"
 # =========================== RELEASE ==========================================
 echo '--> Release'
 echo " -- Zip"
-cd build/Release
+cd build/Release || exit
 zip -r -y ~/Desktop/MongoDB.zip MongoDB.app
-cd ../../
+cd ../../ || exit
 
 # Get zip file size
 FILE_SIZE=$(du ~/Desktop/MongoDB.zip | cut -f1)
@@ -114,7 +114,7 @@ category: release
 ---
 
 - Updates mongodb to $VERSION
-" > ./_posts/release/$(date +"%Y-%m-%d")-${RELEASE_VERSION}.md
+" > "./_posts/release/$(date +"%Y-%m-%d")-${RELEASE_VERSION}.md"
 
 
 # =========================== PUBLISH ==========================================
